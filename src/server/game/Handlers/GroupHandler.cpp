fb43ed7b0a3b4298a6fafe126756725dec0a38a6
tyraela <wow@codeart.ch>
2014-07-14 19:53:35 +0200

    SendPartyResult (SMSG_PARTY_COMMAND_RESULT) changed for 2.4.3

--- src/server/game/Handlers/GroupHandler.cpp
+++ src/server/game/Handlers/GroupHandler.cpp
@@ -50,9 +50,8 @@ void WorldSession::SendPartyResult(PartyOperation operation, const std::string& 
 {
     WorldPacket data(SMSG_PARTY_COMMAND_RESULT, 4 + member.size() + 1 + 4 + 4);
     data << uint32(operation);
-    data << member;
+    data << member;                             // max len 48
     data << uint32(res);
-    data << uint32(val);                                    // LFD cooldown related (used with ERR_PARTY_LFG_BOOT_COOLDOWN_S and ERR_PARTY_LFG_BOOT_NOT_ELIGIBLE_S)
 
     SendPacket(&data);
 }
